60a385007603110a3860d73dd1550f2a4e1d4091
Gustavo de Assis Costa <gacosta@gmail.com>
2024-06-14 11:15:40 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,97 @@
-from openai import OpenAI
+# import dependencies
+import os
+rom openai import OpenAI
 import streamlit as st
+
+from langchain.chains import RetrievalQA
+from langchain.prompts import SystemMessagePromptTemplate
+from langchain.prompts import HumanMessagePromptTemplate
+from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+from langchain_community.vectorstores import Neo4jVector
+from langchain.chains import RetrievalQAWithSourcesChain
+from neo4j import GraphDatabase
+from langchain_community.graphs.neo4j_graph import Neo4jGraph
+
+# Global constants
+VECTOR_INDEX_NAME = 'chunk_embedding'
+VECTOR_NODE_LABEL = 'Chunk'
+VECTOR_SOURCE_PROPERTY = 'text'
+VECTOR_EMBEDDING_PROPERTY = 'textEmbedding'
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+graph = Neo4jGraph(
+    url=NEO4J_URI,
+    username=NEO4J_USERNAME,
+    password=NEO4J_PASSWORD
+)
+
+#embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
+from langchain.embeddings import OpenAIEmbeddings
+embeddings = OpenAIEmbeddings()
+
+llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+
+def configure_qa_structure_rag_chain(llm, embeddings, embeddings_store_url, username, password):
+    # RAG response based on vector search and retrieval of structured chunks
+
+    general_system_template = """Use as seguintes partes do contexto para responder à pergunta no final.
+    Suas respostas deve ser completas e estruturadas.
+    Se você não sabe a resposta, apenas diga que não sabe, não tente inventar uma resposta.
+
+    {summaries}
+
+    Question: {question}
+
+    Helpful Answer:"""
+    general_user_template = " Question: {question}"
+    messages = [
+        SystemMessagePromptTemplate.from_template(general_system_template),
+        HumanMessagePromptTemplate.from_template(general_user_template),
+    ]
+    qa_prompt = ChatPromptTemplate.from_messages(messages)
+
+    qa_chain = load_qa_with_sources_chain(
+        llm,
+        chain_type="stuff",
+        prompt=qa_prompt,
+    )
+
+    # Initialise Neo4j as Vector + Knowledge Graph store
+    kg = Neo4jVector.from_existing_index(
+        embedding=embeddings,
+        url=embeddings_store_url,
+        username=username,
+        password=password,
+        database='neo4j',  # neo4j by default
+        index_name="embedingIndex",  # vector index name
+        node_label="Chunk",  # embedding node label
+        embedding_node_property="embedding",  # embedding value property
+        text_node_property="text",  # text by default
+        retrieval_query="""
+              WITH node AS embNode, score ORDER BY score DESC LIMIT 10
+              MATCH (embNode) -[:CHUNK_OF]-> (part) -[*]-> (document:Document) WHERE embNode.embedding IS NOT NULL
+              OPTIONAL MATCH (part)-[:SECTION_OF]->(chapter:Chapter),(part)-[:CHAPTER_OF]->(document:Document),(part)-[:CHAPTER_OF]->(title:Title)
+              WITH document, part, embNode, score ORDER BY part.title, embNode.chunk_seq_id ASC
+              WITH document, part, collect(embNode) AS answers, max(score) AS maxScore
+              RETURN {source: document.url, part: part.name, matched_chunk_id: id(answers[0])} AS metadata,
+                  reduce(text = "", x IN answers | text + x.text + '.') AS text, maxScore AS score LIMIT 10;
+        """,
+    )
+
+    kg_qa = RetrievalQAWithSourcesChain(
+        combine_documents_chain=qa_chain,
+        retriever=kg.as_retriever(search_kwargs={"k": 3}),
+        reduce_k_below_max_tokens=False,
+        return_source_documents=True,
+    )
+    return kg_qa
+
+rag_chain = configure_qa_structure_rag_chain(llm, embeddings, embeddings_store_url=NEO4J_URI, username=NEO4J_USERNAME, password=NEO4J_PASSWORD)
 
 st.title("ChatGPT-like clone")
 
@@ -14,20 +106,29 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+        
+# Function for generating LLM response
+def generate_response(prompt_input):
+    input_data = {"question": prompt_input}
+    result = rag_chain.invoke(input_data)
+    return result['answer']
 
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("O que deseja saber?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-        response = st.write_stream(stream)
+#        stream = client.chat.completions.create(
+#            model=st.session_state["openai_model"],
+#            messages=[
+#                {"role": m["role"], "content": m["content"]}
+#                for m in st.session_state.messages
+#            ],
+#            stream=True,
+#        )
+        with st.spinner("Thinking...")
+          response = generate_response(prompt)
+          response = st.write(response)
     st.session_state.messages.append({"role": "assistant", "content": response})
+
