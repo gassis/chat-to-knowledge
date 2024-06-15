af351088b093fdbe50ddca1255356252471d6aa0
Gustavo de Assis Costa <gacosta@gmail.com>
2024-06-14 22:14:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -106,9 +106,7 @@ st.title("Chat-to-Knowledge")
 # Set up memory
 msgs = StreamlitChatMessageHistory(key="langchain_messages")
 if len(msgs.messages) == 0:
-    msgs.add_ai_message("How can I help you?")
-
-view_messages = st.expander("View the message contents in session state")
+    msgs.add_ai_message("Como posso ajudá-lo(a)?")
 
 
 chain_with_history = RunnableWithMessageHistory(
@@ -124,20 +122,10 @@ for msg in msgs.messages:
 
 # If user inputs a new prompt, generate and draw a new response
 if prompt := st.chat_input():
-    st.chat_message("human").write(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
     # Note: new messages are saved to history automatically by Langchain during run
     config = {"configurable": {"session_id": "any"}}
     response = chain_with_history.invoke({"question": prompt}, config)
     st.chat_message("ai").write(response['answer'])
-
-# Draw the messages at the end, so newly generated ones show up immediately
-with view_messages:
-    """
-    Message History initialized with:
-    ```python
-    msgs = StreamlitChatMessageHistory(key="langchain_messages")
-    ```
-
-    Contents of `st.session_state.langchain_messages`:
-    """
-    view_messages.json(st.session_state.langchain_messages)
